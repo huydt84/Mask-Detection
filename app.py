c9d29bcedd49a9ee1d7f9ec16254491e13900042
Đinh Trọng Huy <77562200+huydt84@users.noreply.github.com>
2021-12-30 23:32:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	return (locs, preds)
 
 # load our serialized face detector model from disk
-prototxtPath = "face_detector\\deploy.prototxt"
+prototxtPath = "face_detector\\deploy.prototxt.txt"
 weightsPath = "face_detector\\res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
